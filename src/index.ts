3c574a43872e47437056c0a55f586416017f4ed7
Onesmus <okonkwoonesmus5@gmail.com>
2024-03-30 14:18:30 +0000

    chore: display duration and add build script

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { Command } from "commander";
 import { getPdfFiles, fetchWebPageAsText } from "./utils";
 
 const program = new Command();
-
+const startTime = Date.now();
 program
 .option('-u --url','the url to site to process')
 .argument('<url>', '<url> the url to site to process')
 .action( async (url: string) => {
     const websiteAstext = await fetchWebPageAsText(url);
     const pdfFiles = await getPdfFiles(websiteAstext);
-    console.log(pdfFiles);
+    console.log(pdfFiles, 'duration:', Date.now() - startTime + 'ms');
 }).description("this command take in an arg url for a website url process it in order to get all pdf file in the page and merge them ");   
 
 
